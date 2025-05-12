41aea3da0ed879d9ea014db2747f5b6a7b4e9e32
botkomakers <siam420riiya@gmail.com>
2025-05-12 19:52:13 +0600

    Update script.py

--- Youtube/script.py
+++ Youtube/script.py
@@ -1,15 +1,1 @@
-class Translation(object):
-
-    START_TEXT = '**Hello,** **{0}**  **{1}**\n\n**Send me the YouTube link of the video you want to upload**' 
-
-    ABOUT_TXT = """
-╭───────────⍟
-├📛 **My Name** : [YouTube Video Uploader Bot](https://t.me/YouTubeUploaderOneBot)
-├📢 **Framework** : [PyroFork 2.3.50](https://docs.pyrogram.org/)
-├💮 **Language** : [Python 3.12.6](https://www.python.org)
-├👥 **Support Group** : [NT BOTS SUPPORT](https://t.me/NT_BOTS_SUPPORT)
-├🥏 **Channel** : [NT BOT CHANNEL](https://t.me/NT_BOT_CHANNEL)
-├⛲ **Source** : [Click Here](https://github.com/LISA-KOREA/YouTube-Video-Download-Bot)
-├🎓 **Developer** : [LISA 👑](https://t.me/LISA_FAN_LK)
-╰───────────────⍟
-"""
+#ণ
